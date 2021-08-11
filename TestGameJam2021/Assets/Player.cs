53413485c2f8468f74c08f4d91b1ade1f70c18f4
DifolderyXXL <87066000+DifolderyXXL@users.noreply.github.com>
2021-08-11 16:35:44 +0300

    bugs

--- TestGameJam2021/Assets/Player.cs
+++ TestGameJam2021/Assets/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
    [SerializeField] private Rigidbody2D _rigidbody;
    [SerializeField] private float _speed;
-   [SerializeField] private float _jumpforse;
+   [SerializeField] private float _jumpforсe;
    [SerializeField] private Transform _checkPoint;
    [SerializeField] private LayerMask _layer;
    [SerializeField] private float _range;
@@ -17,13 +17,13 @@ public class Player : MonoBehaviour
    }
    private void Update() {
        float movement = Input.GetAxis("Horizontal");
-       _rigidbody.velocity = new Vector2(movement,0) * _speed;
-       bool _isOnEarth = Physics2D.OverlapCircle(_checkPoint.position,_range, _layer);
+
+       _rigidbody.velocity = new Vector2(movement * _speed, _rigidbody.velocity.y);
+       bool _isOnEarth = Physics2D.OverlapCircle(_checkPoint.position, _range, _layer);
        
-       if (Input.GetKeyDown(KeyCode.Space) && _isOnEarth ==true)
-       {
+       if (Input.GetKeyDown(KeyCode.Space) && _isOnEarth)
           Jump();
-       }
+       
        
        if(movement != 0)
        {
@@ -34,7 +34,7 @@ public class Player : MonoBehaviour
 
 
    }
-   private void Jump() => _rigidbody.AddForce(Vector2.up*_jumpforse);
+   private void Jump() => _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpforсe);
    
    private void ChangeDirection() {
        float _turn = _isTurned ?_sizes.x : -_sizes.x;
